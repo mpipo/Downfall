0ca050629b4a3055b59ddaf41bf339655d41b8e3
MrHushbrown <assassindovahkiin99@gmail.com>
2016-04-08 10:32:53 -0700

    drew pre-planning

--- src/downfall/Map.java
+++ src/downfall/Map.java
@@ -22,7 +22,7 @@ class Map extends Environment {
 
     public Map() {
         this.state = GameState.MENU;
-        this.setBackground(Color.black);
+        this.setBackground(Color.white);
     }
 
     @Override
@@ -53,11 +53,15 @@ class Map extends Environment {
     @Override
     public void paintEnvironment(Graphics graphics) {
 
-        graphics.drawRect(350, 0, 400, 670);
-        graphics.drawRect(850, 0, 400, 670);
-        graphics.drawRect(-10, 470, 300, 200);
+        graphics.drawRect(850, 0, 500, 800);
+        graphics.drawRect(1400, 0, 500, 800);
+        graphics.drawRect(-10, 400, 700, 400);
         
-        graphics.setColor(Color.yellow);
+        graphics.setColor(Color.magenta);
+        graphics.drawRect(0, 800, 1920, 200);
+        
+        graphics.setColor(Color.red);
+        graphics.drawRect(450, 635, 80, 165);
 
     }
 
