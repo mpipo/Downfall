aed8f593e9fdda2fb53cf4827bc2b38158f95fe9
Ratchaphon Manosuthi <themelo64@gmail.com>
2016-04-26 19:52:41 -0700

    I don't know what I'm doing

--- src/downfall/Player.java
+++ src/downfall/Player.java
@@ -37,21 +37,18 @@ public class Player {
 
     }
 //</editor-fold>
-<<<<<<< HEAD
-=======
     
      //<editor-fold defaultstate="collapsed" desc="properties">
     private Direction direction = Direction.RIGHT;
     private ArrayList<Point> body;
     private Color BodyColor = Color.GREEN;
     private int mouthWidth;
->>>>>>> refs/remotes/origin/master
+
 
     //<editor-fold defaultstate="collapsed" desc="properties">
     private int x, y;
     private int width, height;
     private int health = 100;
-    private Direction direction = Direction.RIGHT;
 //    private Color color;
 //    private double speed = 11.25;
     private Velocity velocity;
